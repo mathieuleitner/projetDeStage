cb34312264d79f4eb17b1174c1669c6e57892cfd
Leitner Mathieu <mleitner@SQLI40648.SQLI>
2018-11-23 17:40:18 +0100

    wip

--- app/js/yeuxMickey.js
+++ app/js/yeuxMickey.js
@@ -2,12 +2,23 @@ $(document).ready(function() {
     console.log('ready')
 
     $('#manor').mousemove(function(event){
-        var xPos = event.pageX/10-40;
-        var yPos = event.pageY/10-110;
+        var xPos = event.pageX / $(window).width() * 100 - 8;
+        var yPos = event.pageY / $(window).height() * 100 - 120;
+
+        if (yPos < 10){
+            yPos = 10;
+        }
+
+        else if ( yPos > 90 ){
+            yPos = 90;
+        }
+
         $('.manor--mickey--tete--yeux--pupille').css({
             'left' : xPos + '%',
             'top' : yPos + '%'
         });
+        console.log('yPos = ' + yPos);
+        console.log('xPos = ' + xPos);
        
     });
 
